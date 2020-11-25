dbb7ea04ca92c69aab24de4e7dd927bc29677783
jirvingphd <james.irving.phd@gmail.com>
2020-11-25 11:44:52 -0500

    Made new app

--- app.py
+++ app.py
@@ -2,27 +2,53 @@
 import os
 from random import randint
 
-import plotly.plotly as py
-from plotly.graph_objs import *
-
 import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
 import dash_core_components as dcc
 import dash_html_components as html
-
+import plotly.express as px
+import pandas as pd
 
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
-server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+
+app = dash.Dash(__name__)
+server = app.server
 
 
 # Put your Dash code here
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    #app.server.run(debug=True, threaded=True)
+    app.run_server(debug=True)
